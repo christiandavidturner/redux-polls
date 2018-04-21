dfe811a3f440bdb09e49cf7d947da50ff2975639
christian <christiandavidphoto@gmail.com>
2018-04-21 19:40:41 -0400

    fixes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
-import polls from './polls';
-import users from './users';
+import { combineReducers } from 'redux';
 import authedUser from './authedUser';
+import users from './users';
+import polls from './polls';
 
-export default combineReducers(authedUser, users, polls);
+export default combineReducers({
+  authedUser,
+  users,
+  polls
+});
