3309f41c6acfec6899a07b474365a7856f2c2106
christian <christiandavidphoto@gmail.com>
2018-04-21 21:25:47 -0400

    loading-bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,17 +2,20 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from '../actions/users';
 import { receivePolls } from '../actions/polls';
 import { setAuthedUser } from '../actions/authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'tylermcginnis';
 
 //thunk
 export function handleInitialData() {
   return dispatch => {
+    dispatch(showLoading());
     // make async requests here
     return getInitialData().then(({ users, polls }) => {
       dispatch(receiveUsers(users));
       dispatch(receivePolls(polls));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 }
